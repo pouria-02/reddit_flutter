298de19abeaff7e3e2ae32ac3a18d1a44000e170
Negin <neg.qorbani@gmail.com>
2022-05-30 15:55:57 +0430

    class post

--- lib/Post.dart
+++ lib/Post.dart
@@ -1,8 +1,21 @@
 class Post {
-  late String userName;
+  late String societyName;
+  late String  description;
+  late String title;
   DateTime dateTime = DateTime.now();
   late int LikeCount ;
   late int DisLikeCount;
-
+  int get likes => LikeCount ;
+  int get dislikes => DisLikeCount ;
+  void setlikes (){
+    LikeCount ++;
+  }
+  void setDislikes (){
+    DisLikeCount ++;
+  }
+  set setDescription (String s ){
+    description = s ;
+  }
+  String get Description => description;
 
 }

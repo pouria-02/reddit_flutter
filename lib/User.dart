3242e1f8afd35a31ea086872453cbd31a52fc3b2
Negin <neg.qorbani@gmail.com>
2022-06-01 19:14:16 +0430

    class user

---
+++ lib/User.dart
@@ -0,0 +1,11 @@
+import 'Post.dart';
+
+class User{
+  late String userName;
+  late String password;
+  late Object profile;
+  //String? bio;
+  late List<Post> postsOfUser;
+  // List<Community> communities = [];
+
+}
